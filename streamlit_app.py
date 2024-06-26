4c2605481c6dfb246609120b4978b230545a6567
cced3000 <zqjilove@qq.com>
2024-06-26 14:47:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import requests
 
 # Streamlit 应用的标题
-st.title("抖音主播图片显示")
+st.title("AIbase-职业/人物生成器")
 
 # 创建一个输入框让用户输入内容
-inputs = st.text_input("请输入主播名称", "抖音主播李佳琪")
+inputs = st.text_input("请输入职业/人物", "ai prompt Engineer")
 
 # 当用户点击按钮时执行以下代码
 if st.button("获取图片"):
@@ -13,7 +13,7 @@ if st.button("获取图片"):
         # 调用 API 获取数据
         response = requests.post(
             "https://simple-api.glif.app",
-            json={"id": "clxv8wwhj0000b3f5shjg1221q3xy", "inputs": ["抖音主播李佳琪"]},
+            json={"id": "clxv8wwhj0000b3f5shjgq3xy", "inputs": [inputs]},
             headers={"Authorization": "Bearer b54c3ea9ec3f0b518d14152a5db90995"},
         )
         
